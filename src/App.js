39a60523e8984dd81b7d3c6fe3b8421064c5aa72
Dheeraj-parashar <127424719+Dheeraj-parashar@users.noreply.github.com>
2023-06-12 11:03:01 +0530

    footer in progress

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Navbar from './components/Navbar';
 import Slider from './components/Slider';
 import Slider2 from './components/Slider2';
 import DailyDeal from './components/DailyDeal';
-
+import Footer from './components/Footer';
+import FooterElement from './components/FooterElements';
 function App() {
   return (
     <div className="App">
@@ -15,6 +16,8 @@ function App() {
       <Slider2/>
       <BestSeller/>
       <DailyDeal/>
+      <Footer/>
+      <FooterElement/>
           </div>
   );
 }
---
+++ src/components/FooterElements.js
@@ -0,0 +1,90 @@
+import React from "react";
+export default function FooterElement(){
+    return(
+        <div>
+            <div className="mobile_footer_item_title"><div className="menu-panel">
+                <button id="link_btn_1" className="footer__item-title heading heading--small">
+                  Shop
+                
+  <span className="expanded toggle">+</span>
+  <span className="collapsed toggle visually-hidden">-</span>
+  </button>
+                <div className="footer__item-content footer__item-mobile footer_item_1">
+                  <ul className="linklist list--unstyled" ><li className="linklist__item">
+                        <a href="/collections/true-wireless-earbuds" className="link--faded">True Wireless Earbuds</a>
+                      </li><li className="linklist__item">
+                        <a href="/collections/bluetooth-wireless-headphones" className="link--faded">Wireless Headphones</a>
+                      </li><li className="linklist__item">
+                        <a href="/collections/wired-headphones" className="link--faded">Wired Headphones</a>
+                      </li><li className="linklist__item">
+                        <a href="/collections/wireless-speakers" className="link--faded">Wireless Speakers</a>
+                      </li><li className="linklist__item">
+                        <a href="/collections/home-audio" className="link--faded">Home Audio</a>
+                      </li><li className="linklist__item">
+                        <a href="/collections/mobile-accessories" className="link--faded">Mobile Accessories</a>
+                      </li><li className="linklist__item">
+                        <a href="/collections/smart-watches" className="link--faded">Smart Watches</a>
+                      </li><li className="linklist__item">
+                        <a href="/collections/trebel-for-women" className="link--faded">TRebel</a>
+                      </li><li className="linklist__item">
+                        <a href="/collections/misfit-by-boat" className="link--faded">Misfit</a>
+                      </li><li className="linklist__item">
+                        <a href="https://www.boat-lifestyle.com/products/gift-card" className="link--faded">Gift Card</a>
+                      </li><li className="linklist__item">
+                        <a href="https://www.boat-lifestyle.com/pages/earn-rewards-referring-friends?utm_source=referralprogram&amp;utm_medium=referral_footer&amp;utm_campaign=referralprogram" className="link--faded">Earn ₹100</a>
+                      </li></ul>
+                </div>
+              </div><div className="menu-panel">
+                <button id="link_btn_2" className="footer__item-title heading heading--small">
+                  Help
+                
+  <span className="expanded toggle">+</span>
+  <span className="collapsed toggle visually-hidden">-</span>
+  </button>
+                <div className="footer__item-content footer__item-mobile footer_item_2">
+                  <ul className="linklist list--unstyled" ><li className="linklist__item">
+                        <a href="https://dtc.boat-lifestyle.com/track" className="link--faded">Track Your Order</a>
+                      </li><li className="linklist__item">
+                        <a href="http://support.boat-lifestyle.com/" className="link--faded">Warranty &amp; Support</a>
+                      </li><li className="linklist__item">
+                        <a href="/pages/return-policy" className="link--faded">Return Policy</a>
+                      </li><li className="linklist__item">
+                        <a href="/pages/service-center-list" className="link--faded">Service Centers</a>
+                      </li><li className="linklist__item">
+                        <a href="mailto:Corporate@imaginemarketingindia.com?subject=BoAt Bulk Orders&amp;body=Hi,%0d%0a%0d%0aI would like to request a bulk shipment of boAt products.%0d%0a%0d%0aDetails of boAt products required:%0d%0a%0d%0aContact Phone Number:%0d%0a%0d%0a" className="link--faded">Bulk Orders</a>
+                      </li><li className="linklist__item">
+                        <a href="/pages/frequently-asked-questions" className="link--faded">FAQs</a>
+                      </li><li className="linklist__item">
+                        <a href="/pages/why-buy-direct" className="link--faded">Why Buy Direct</a>
+                      </li></ul>
+                </div>
+              </div><div className="menu-panel">
+                <button id="link_btn_3" className="footer__item-title heading heading--small">
+                  Company
+                
+  <span className="expanded toggle">+</span>
+  <span className="collapsed toggle visually-hidden">-</span>
+  </button>
+                <div className="footer__item-content footer__item-mobile footer_item_3">
+                  <ul className="linklist list--unstyled" ><li className="linklist__item">
+                        <a href="/pages/who-are-we" className="link--faded">About boAt</a>
+                      </li><li className="linklist__item">
+                        <a href="https://www.boat-lifestyle.com/blogs/news" className="link--faded">News</a>
+                      </li><li className="linklist__item">
+                        <a href="/blogs/blog" className="link--faded">Read Our Blog</a>
+                      </li><li className="linklist__item">
+                        <a href="mailto:Careers@imaginemarketingindia.com?subject=Hi boAthead" className="link--faded">Careers</a>
+                      </li><li className="linklist__item">
+                        <a href="/pages/security" className="link--faded">Security</a>
+                      </li><li className="linklist__item">
+                        <a href="/pages/investor-relations" className="link--faded">Investor Relations</a>
+                      </li><li className="linklist__item">
+                        <a href="https://www.boat-lifestyle.com/pages/social-responsibility" className="link--faded">Social Responsibility</a>
+                      </li><li className="linklist__item">
+                        <a href="https://www.boat-lifestyle.com/pages/warranty" className="link--faded">Warranty Policy</a>
+                      </li></ul>
+                </div>
+              </div></div>
+        </div>
+    )
+}
